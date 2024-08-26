e7caeffc4e43ed2db219e5293f2996e9491cf02c
GunnAtlas <yaumama5@gmail.com>
2024-08-25 18:36:03 -0700

    hellllllllllllooooo

---
+++ index.js
@@ -0,0 +1,12 @@
+let icons = document.querySelectorAll(".icon")
+
+for (let i = 0; i < icons.length; i++) {
+    icons[i].onclick = function() {
+        if(!icons[i].classList.contains("selected")) {
+            for (let o = 0; o < icons.length; o++) {
+                icons[o].classList.remove("selected")
+            }
+            icons[i].classList.add("selected")
+        }
+    }
+}
