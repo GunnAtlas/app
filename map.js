6e1843de32e361e8b8068673811435bcdd1a3e00
Yaumama <yaumama1@hotmail.com>
2024-09-04 19:42:43 -0700

    HLELELO

---
+++ map.js
@@ -0,0 +1,8 @@
+var mc = new Hammer.Manager(document.querySelector(".map"))
+var pinch = new Hammer.Pinch()
+
+mc.add([pinch])
+
+mc.on("pinch", function(ev) {
+    document.querySelector(".hi").innerText = ev
+})
