23533d94665f31c067745a7fd776b7c8633f0a4d
Yaumama <yaumama1@hotmail.com>
2024-09-05 17:06:41 -0700

    HLELELO

--- map.js
+++ map.js
@@ -4,5 +4,5 @@ var pinch = new Hammer.Pinch()
 mc.add([pinch])
 
 mc.on("pinch", function(ev) {
-    document.querySelector(".hi").innerText = Object.keys(ev.scale).length
+    document.querySelector(".hi").innerText = ev.scale
 })
